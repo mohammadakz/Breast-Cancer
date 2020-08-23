15d1ff22f1b9a6b798dd6d8062afa97d96f2f9be
Mohammad Akbarzadeh <akbarzadehm1996@gmail.com>
2020-08-23 17:48:10 -0400

    Fixed the line for reading dataset

--- Breast Cancer logistic regression.py
+++ Breast Cancer logistic regression.py
@@ -2,16 +2,26 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 # Reading the dataset
 dataset = pd.read_csv('breast_cancer.csv')
 # index locating
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Train and test dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Training the model
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
+
+# Predicting the test results
+y_pred = classifier.predict(X_test)
+
+# Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+acc = accuracy_score(y_test, y_pred)
+print(acc)
