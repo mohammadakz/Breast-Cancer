39ae6a248a43bf1d8e81ac0562a97bf2af2a5256
Mohammad Akbarzadeh <akbarzadehm1996@gmail.com>
2020-08-23 17:41:16 -0400

    Training the model

--- Breast Cancer logistic regression.py
+++ Breast Cancer logistic regression.py
@@ -1,8 +1,17 @@
 # Importing libraries
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 # Reading the dataset
 dataset = pd.read_csv('breast_cancer.csv')
 # index locating
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+# Train and test dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Training the model
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
